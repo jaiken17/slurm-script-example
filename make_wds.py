11457d2947fdf85bf1f03689039cdc93ebc7734a
jaiken17 <jaiken17@ubishops.ca>
2025-05-16 17:33:11 -0400

    build slurm array job submission script

--- make_wds.py
+++ make_wds.py
@@ -24,32 +24,44 @@ def copy_dir(template_dir: str, new_wd: str):
     shutil.copytree(template_dir,new_wd) # copies template_dir to new_wd
 
 
-def main(params_file: str, template_dir: str):
+def main(params_file: str, template_dir: str, index: int):
 
-    with open(params_file, 'r') as file:
-        for line in file:
-            entries = line.strip().split()
-            new_wd = entries[0]
-            params = entries[1:]
-            if len(params) != len(param_placeholders):
-                raise Exception(f'Found {len(params)} parameters; expected {len(param_placeholders)}')
-            copy_dir(template_dir,new_wd)
-            replace_params(new_wd, params)
+    file = open(params_file,'r')
+    lines = file.readlines()
+    file.close()
+    line = lines[index]
+    
+    entries = line.strip().split()
+    new_wd = entries[0]
+    params = entries[1:]
+    if len(params) != len(param_placeholders):
+        raise Exception(f'Found {len(params)} parameters; expected {len(param_placeholders)}')
+    copy_dir(template_dir,new_wd)
+    replace_params(new_wd, params)
+
+    print(new_wd)
 
 
-def get_command_line_args() -> (str,str):
+def get_command_line_args() -> (str,str,int):
     args = sys.argv
     args = args[1:]
-    num_args_expected = 2
+    num_args_expected = 3
     if len(args) != num_args_expected:
         raise Exception(f'{len(args)} command line args provided; {num_args_expected} command line args expected.')
     params_file = args[0]
     template_dir = args[1]
+    array_index = args[2] 
+    try:
+        array_index = int(array_index)
+        array_index -= 1 # note slurm array is 1-indexed (starts at 1 not 0)
+    except ValueError:
+        print("Expected array index to be integer")
 
-    return (params_file, template_dir)
+
+    return (params_file, template_dir, array_index)
 
 
 if __name__ == '__main__':
 
-    (params_file,template_dir) = get_command_line_args()
-    main(params_file, template_dir)
+    (params_file,template_dir,index) = get_command_line_args()
+    main(params_file, template_dir, index)
--- slurm_start.sh
+++ slurm_start.sh
@@ -1,6 +1,30 @@
 #!/bin/bash
+#!/bin/bash"
+#SBATCH --time=00:05:00
+##SBATCH --account=#{groupName}
+#SBATCH --job-name=example_job
+#SBATCH --output=output-%x_%j.out
+#SBATCH --error=error-%x_%j.out
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=1G
+#SBATCH --array=1-3
+##SBATCH --mail-user=<#{email}>
+##SBATCH --mail-type=END
+##SBATCH --mail-type=FAIL
 
+# two '#' means SLURM skips command
 
-
+# Set location of template working directory and parameters file
 TEMPLATE_DIR=template_wd/
 PARAMS_FILE=params.txt
+
+# Call python script that copies template dir into a new run dir and sets initial parameters
+# Python scripts prints the name of new dir to stdout which is saved to $DIR var here
+DIR=$(python3 make_wds.py $PARAMS_FILE $TEMPLATE_DIR $SLURM_ARRAY_TASK_ID)
+
+cd $DIR
+
+# Copy the script that runs the program, and run script with srun (SLURM)
+cp ../prog_run.sh .
+srun ./prog_run.sh
